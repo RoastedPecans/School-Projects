c5a6d558e0bea0c2a26f2a6b320d2caf212807f9
Connor Thompson <connorthompson915@icloud.com>
2017-04-19 15:29:45 -0600

    Updated to Python 3. Creates checkerboard effect using two photos
    See myPILSample.jpeg for an example.

--- Python/PILProject/myPILv2.py
+++ Python/PILProject/myPILv2.py
@@ -1,21 +1,19 @@
 # Creates "Checkerboard" pattern on a new image from two images So every other pixel will be from the other image.
 # This creates a unique image where you can see both of the previous images on the same image
-# Note: Uses Python 2.7
 # Written March 2015
-
 import PIL
 import ctypes
 from PIL import Image
 import PIL.ImageOps
 #Set height and width to size of images 
-userInputWidth = raw_input("Enter the width of your images: ")
-userInputHeight = raw_input("Enter the height of your images: ")
+userInputWidth = input("Enter the desired width for your new image: ")
+userInputHeight = input("Enter the desired height for your new image: ")
 
 height = userInputHeight
 width = userInputWidth
 
-a = 0
-b = 0
+a = 1
+b = 1
 iteration = 0
 value = (0, 0, 0 , 1)
 #Setup images and make them RGBA and the same size
@@ -23,24 +21,23 @@ img1 = Image.open("IMAGE1.jpg")
 img1 = img1.convert("RGBA")
 img1 = img1.resize((int(userInputWidth), int(userInputHeight)), Image.ANTIALIAS)
 
-img2 = Image.open("IMAGETWO.jpg")
+img2 = Image.open("IMAGE2.jpg")
 img2 = img2.convert("RGBA")
 img2 = img2.resize((int(width), int(height)), Image.ANTIALIAS
 )
 
-
 pix = img1.load()
-print img1.size
+#print img1.size
 
 #Set height and width to size of images (Failsafe...)
 [height, width] = img1.size
 
 #Prints the Height and the Width of the Image
-print "Height = " + str(height) + " Width = " + str(width) 
+#print "Height = " + str(height) + " Width = " + str(width)
 
 #Cycles through all pixels in the image
 while a < height and b < width:
-    print a
+    #print a
 
     #Sets checkerboard to pixels from second image
     if a % 2 == 0 or a == 0 or a == userInputHeight:
@@ -55,9 +52,10 @@ while a < height and b < width:
         rgb = img2.getpixel((a, b))
         img1.putpixel((a, b), rgb)
         iteration = iteration + 1
-        print 'ITERATION = ' + str(iteration)
+        #print 'ITERATION = ' + str(iteration)
         a = a - int(userInputWidth) + 1
         b = b + 1
         
             
 img1.show()
+
